d0233fa8440a1c0839b336b3ea4bfb8b91455cc7
mamixx15 <fahmianis05@gmail.com>
2026-07-03 17:51:12 +0700

    ya

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  // Analytics - track page views
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // This is where you could add analytics tracking
+      console.log(`Page navigated to: ${url}`);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
